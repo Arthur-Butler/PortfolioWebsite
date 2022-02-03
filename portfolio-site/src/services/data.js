6bccf923fce47aee6f0e1fabbed6f60d01eceb67
Arthur-Butler <62343020+Arthur-Butler@users.noreply.github.com>
2022-02-03 13:11:16 +0200

    Deleted data.js

--- portfolio-site/src/services/data.js
+++
@@ -1,6 +0,0 @@
-export const data = {
-    name:"Arthur Nyaniso Butler",
-    occupation:"Back-end Software Engineer",
-    description:"ABSA Younglings and Capaciti Associate",
-    image:"Capaciti.png"
-}
